78a767e7c831763d054496008af72a226076d4c5
seninhacalvo <125033666+seninhacalvo@users.noreply.github.com>
2023-05-19 16:15:38 +0000

    inplementação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,5 +9,16 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  calendario = {
+    mes:'Maio',
+    ano:'2023',
+    foto:'https://images.pexels.com/photos/15499724/pexels-photo-15499724/free-photo-of-balao-bexiga-globo-entretenimento.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    dias_da_semana:['DOM,SEU,YER,QUA,QUI,SEX,SAB'],
+    primeira_semana:['30,1,2,3,4,5,6,'],
+    segunda_semana:['7,8,9,10,11,12,13'],
+    terceira_semana:['14,15,16,17,18,19,20'],
+    querta_semana:['21,22,23,24,25,26,27'],
+    quinta_semana:['29,29,30'],
+
+  }
 }
