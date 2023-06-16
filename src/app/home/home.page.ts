eaaf75db8f2918c7ee8e5fa0419df46d7185b2e5
seninhacalvo <125033666+seninhacalvo@users.noreply.github.com>
2023-06-16 15:52:01 +0000

    Quase no fim da Calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -7,11 +8,56 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule,CommonModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
- 
-	exibir(numero: number) {
-		console.log(numero);
-	}
+
+  // valores
+  valor1: any = null;
+  valor2: any = null;
+  resultado: any = null;
+  operador: any = null;
+
+  exibir(numero: any) {
+
+    if (this.valor1 == null) {
+      this.valor1 = numero;
+      this.resultado = numero;
+    } else {
+      if (this.valor2 == null) {
+        this.valor2 = numero;
+        this.resultado = numero;
+      }
+
+    }
+
+    // mostrar os consoles
+    console.log(this.valor1);
+    console.log(this.valor2);
+    console.log(this.operacao);
+  }
+
+  // Operações do logicas
+  operacao(operador: any) {
+    this.operador = operador;
+  }
+
+
+  calcular() {
+    if (this.operador == '+') {
+      this.resultado = this.valor1 + this.valor2;
+    }
+    else if (this.operador == '-') {
+      this.resultado = this.valor1 - this.valor2;
+    }
+    else if (this.operador == '*') {
+      this.resultado = this.valor1 * this.valor2;
+    }
+    else if (this.operador == '/') {
+      this.resultado = this.valor1 / this.valor2;
+    }
+    else if (this.operador == '%') {
+      this.resultado = this.valor1 % this.valor2;
+    }
+  }
 }
